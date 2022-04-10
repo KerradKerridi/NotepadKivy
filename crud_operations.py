45473434dc9871ab80096ee13874fb597c76ef69
KerradKerridi <KerradKerridi>
2022-04-10 19:51:32 +0300

    v 1.1

--- crud_operations.py
+++ crud_operations.py
@@ -1,9 +1,13 @@
 import os
 
+root_path = '/opt/python/PycharmProjects/NotepadForLinux/notebooks/'
+
 def save_note(self, header, body):
     Body = body.text
     Header = header.text
-    os.chdir('/opt/python/PycharmProjects/NotepadForLinux/notebooks')
+    #TODO: Добавить проверку на необходимость создания папки, если нет такой папки у пользователя
+    #TODO: Добавить реализацию через относительные пути
+    os.chdir(root_path)
     f = open(f'{Header}.txt', 'w', encoding='utf-8')
     f.write(Body)
     f.close()
@@ -12,7 +16,27 @@ def save_note(self, header, body):
 
 def delete_note(self, header):
     Header = header.text
-    root_path = '/opt/python/PycharmProjects/NotepadForLinux/notebooks/'
     pathtodel = os.path.join(os.path.abspath(root_path), f'{Header}.txt')
     os.remove(pathtodel)
     print('Success')
+
+def read_notes(self):
+    #TODO: Сделать через один массив массивов или json
+    files = os.listdir(root_path)
+    headers = []
+    strings = []
+
+    for file in files:
+        headers.append(file)
+
+    lenMassive = len(headers)
+
+    for file in files:
+        f = open(f'{root_path}{file}', 'r', encoding='utf-8')
+        string = f.read()
+        strings.append(string)
+        f.close()
+
+    print(headers)
+    print(strings)
+    print(lenMassive)
--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
 import crud_operations
-
-
 
 kv = Builder.load_file("windows.kv")
 sm = ScreenManager()
 
 class MainWidget(Screen):
     pass
-
 
 class EditTextWidget(Screen):
     #TODO: Если заметка пустая(заголовок и текст), не давать ее сохранять, выводить модалку об ошибке
@@ -25,9 +24,13 @@ class EditTextWidget(Screen):
 class EmptyPage(Screen):
     pass
 
+class FirstWindow(Screen):
+    pass
+
 class MainApp(App):
 
     def build(self):
+        sm.add_widget(FirstWindow(name='first'))
         sm.add_widget(MainWidget(name='main'))
         sm.add_widget(EditTextWidget(name='edit'))
         sm.add_widget(EmptyPage(name='empty'))
@@ -35,5 +38,3 @@ class MainApp(App):
 
 if __name__ == '__main__':
     MainApp().run()
-
-#Откат на 13-08
