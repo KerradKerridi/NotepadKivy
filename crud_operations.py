c0964d2ffea41bcc755240cc1350611f4c214087
KerradKerridi <KerradKerridi>
2022-04-12 22:13:17 +0300

    fix bugs

--- crud_operations.py
+++ crud_operations.py
@@ -2,26 +2,41 @@ import os
 
 root_path = '/opt/python/PycharmProjects/NotepadForLinux/notebooks/'
 
-def save_note(self, header, body):
-    Body = body.text
-    Header = header.text
-    #TODO: Добавить проверку на необходимость создания папки, если нет такой папки у пользователя
-    #TODO: Добавить реализацию через относительные пути
+def save_edit_note(header, body):
+    body = body.text
+    header = header.text
+    # TODO: Добавить проверку на необходимость создания папки, если нет такой папки у пользователя
+    # TODO: Добавить реализацию через относительные пути
+    # TODO: Если нет окончания txt то добавлять, либо сохранять в config, как в test.py
     os.chdir(root_path)
-    f = open(f'{Header}.txt', 'w', encoding='utf-8')
-    f.write(Body)
+    f = open(f'{header}', 'w', encoding='utf-8')
+    f.write(body)
     f.close()
-    print(Header)
-    print(Body)
+    print(header)
+    print(body)
 
-def delete_note(self, header):
-    Header = header.text
-    pathtodel = os.path.join(os.path.abspath(root_path), f'{Header}.txt')
-    os.remove(pathtodel)
-    print('Success')
+def delete_edit_note(header):
+    head = header.text
+    path_for_delete = os.path.join(os.path.abspath(root_path), f'{head}')
+    os.remove(path_for_delete)
+    print('SuccessDelete')
 
-def read_notes(self):
-    #TODO: Сделать через один массив массивов или json
+def save_new_note(header, body):
+    body = body.text
+    header = header.text
+    # TODO: Добавить проверку на необходимость создания папки, если нет такой папки у пользователя
+    # TODO: Добавить реализацию через относительные пути
+    os.chdir(root_path)
+    f = open(f'{header}.txt', 'w', encoding='utf-8')
+    f.write(body)
+    f.close()
+    print(header)
+    print(body)
+
+
+
+def read_notes():
+    # TODO: Сделать через один массив массивов или json
     files = os.listdir(root_path)
     headers = []
     strings = []
@@ -29,14 +44,19 @@ def read_notes(self):
     for file in files:
         headers.append(file)
 
-    lenMassive = len(headers)
-
     for file in files:
         f = open(f'{root_path}{file}', 'r', encoding='utf-8')
         string = f.read()
         strings.append(string)
         f.close()
 
-    print(headers)
-    print(strings)
-    print(lenMassive)
+    return headers, strings
+
+def count_notes():
+    files = os.listdir(root_path)
+    headers = []
+    for file in files:
+        headers.append(file)
+
+    len_massive = len(headers)
+    return len_massive
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
 from kivy.properties import ObjectProperty
 import crud_operations
 
@@ -11,15 +11,57 @@ kv = Builder.load_file("windows.kv")
 sm = ScreenManager()
 
 class MainWidget(Screen):
-    pass
+    def __init__(self, **kw):
+        super(MainWidget, self).__init__(**kw)
+
+    def on_pre_enter(self):
+        # TODO: LATER: Добавить вывод текста в кнопке, не только заголовка
+        count_notes = crud_operations.count_notes()
+        head_notes, strings_notes = crud_operations.read_notes()
+        print(count_notes)
+        for i in range(0, count_notes):
+            button = Button()
+            button.text = f'{head_notes[i - 1]}'
+            self.ids.grid.add_widget(button)
+            button.bind(on_press=self.pressed)  # when the button is clicked
+
+    def pressed(self, button):
+        print('successPressedButton')
+        sm.get_screen('edit').ids.HeaderNote.text = button.text
+        sm.current = 'edit'
+
+    def on_leave(self):  # Будет вызвана в момент закрытия экрана
+        self.ids.grid.clear_widgets()
+
 
 class EditTextWidget(Screen):
-    #TODO: Если заметка пустая(заголовок и текст), не давать ее сохранять, выводить модалку об ошибке
-    def save(self, head, body):
-        crud_operations.save_note(self, head, body)
+    # TODO: LATER: Если заметка пустая(заголовок и текст), не давать ее сохранять, выводить модалку об ошибке
+    # TODO: Если заходим в заметку, начинаем ее корректировать, ТО удалять старую заметку
+    def __init__(self, **kw):
+        super(EditTextWidget, self).__init__(**kw)
 
-    def delete(self, head):
-        crud_operations.delete_note(self,head)
+    head = ObjectProperty()
+    body = ObjectProperty()
+
+    def open_files(self):
+        root_path = '/opt/python/PycharmProjects/NotepadForLinux/notebooks/'
+        header = self.ids.HeaderNote.text
+        f = open(f'{root_path}{header}', 'r')
+        s = f.read()
+        self.head.text = header
+        self.body.text = s
+        f.close()
+
+    def save_edit(self, head, body):
+        crud_operations.save_edit_note(head, body)
+
+    def delete_edit(self, head):
+        crud_operations.delete_edit_note(head)
+
+    def on_leave(self):
+        self.head.text = ''
+        self.body.text = ''
+
 
 class EmptyPage(Screen):
     pass
@@ -27,13 +69,21 @@ class EmptyPage(Screen):
 class FirstWindow(Screen):
     pass
 
-class MainApp(App):
+class NewTextWidget(Screen):
+    def save_new(self, head, body):
+        crud_operations.save_new_note(head, body)
 
+    def on_leave(self):
+        self.head.text = ''
+        self.body.text = ''
+
+class MainApp(App):
     def build(self):
         sm.add_widget(FirstWindow(name='first'))
         sm.add_widget(MainWidget(name='main'))
         sm.add_widget(EditTextWidget(name='edit'))
         sm.add_widget(EmptyPage(name='empty'))
+        sm.add_widget(NewTextWidget(name='new'))
         return sm
 
 if __name__ == '__main__':
