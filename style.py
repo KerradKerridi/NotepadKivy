f65a2c019b8c167a5ab77c06754c6a78fe868378
KerradKerridi <KerradKerridi>
2022-04-14 22:41:25 +0300

    v.1.1.3

---
+++ style.py
@@ -0,0 +1,25 @@
+def main_theme_dark():
+    rgba = (.18, .18, .18, 1)
+    return rgba
+
+def main_theme_light():
+    rgba = (.9, .9, .9, 1)
+    return rgba
+
+def input_color_dark():
+    rgba = (.4, .4, .4, 1)
+    return rgba
+
+def input_color_light():
+    rgba = (.85, .85, .85, 1)
+    return rgba
+
+def button_color_dark():
+    #rgba = (.1608, .0, .2471, 1)
+    rgba = (0, 0, 0, 0)
+    return rgba
+
+def button_color_light():
+    #rgba = (100/255, 0/255, 155/255, 1)
+    rgba = (1, 1, 1, 1)
+    return rgba
